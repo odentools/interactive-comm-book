03bd62822c9066510a6f1a6757cb90091e692d36
Masanori Ohgita <mp@ohgita.info>
2016-04-09 05:29:23 +0900

    Change(updater): アップデータを改善

--- devices/general/updater.sh
+++ devices/general/updater.sh
@@ -2,14 +2,29 @@
 
 set -eu
 
-curl https://github.com/odentools/
+# Check the connection
+curl -o /dev/null https://github.com/odentools/
 if [ $? -ne 0 ]; then
-	echo "Could not connect to GitHub"
+	echo "[Updater] Could not connect to GitHub"
 	exit 0
 fi
 
-echo "Updating..."
-git fetch origin master
-git reset --hard FETCH_HEAD
+# Get the current revision
+before_rev=`git show -s --format=%H`
+
+# Update
+echo "[Updater] Checking..."
+#git fetch origin master
+#git reset --hard FETCH_HEAD
+
+# Compare the current revision
+rev=`git show -s --format=%H`
+echo ""
+
+if [ ${rev} != ${before_rev} ]; then
+	echo "[Updater] Updated; New revision is ${rev}"
+else
+	echo "[Updater] Update checked; Current revision is latest"
+fi
 
 exit 0
